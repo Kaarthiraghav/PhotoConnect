f66ba748fd709b7092cc9e789380e3ad2e777b55
Dilki bhanusha <mldbsithari@students.nsbm.ac.lk>
2026-01-22 14:55:24 +0530

    booking

---
+++ src/main/java/com/example/PhotoConnect/booking/controller/BookingController.java
@@ -0,0 +1,56 @@
+package com.example.booking_service.controller;
+
+import com.example.booking_service.model.Booking;
+import com.example.booking_service.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    @PostMapping
+    public Booking createBooking(
+            @RequestParam Long clientId,
+            @RequestParam Long photographerId,
+            @RequestParam String eventDate
+    ) {
+        return bookingService.createBooking(
+                clientId,
+                photographerId,
+                LocalDateTime.parse(eventDate)
+        );
+    }
+
+    @PutMapping("/{id}/accept")
+    public Booking accept(@PathVariable Long id) {
+        return bookingService.acceptBooking(id);
+    }
+
+    @PutMapping("/{id}/reject")
+    public Booking reject(@PathVariable Long id) {
+        return bookingService.rejectBooking(id);
+    }
+
+    @PutMapping("/{id}/cancel")
+    public Booking cancel(@PathVariable Long id) {
+        return bookingService.cancelBooking(id);
+    }
+
+    @PutMapping("/{id}/pay")
+    public Booking pay(@PathVariable Long id) {
+        return bookingService.markAsPaid(id);
+    }
+
+    @GetMapping
+    public List<Booking> getAll() {
+        return bookingService.getAllBookings();
+    }
+}
+
